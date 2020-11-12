204399882dcd1f29e16afd0fef457a5a7400a065
adielmo <adielmorabelo@gmail.com>
2020-11-12 18:14:13 -0300

    Enviando o projeto tecFood. para o gitHub para nuvem

---
+++ src/main/java/com/rabelo/tecfood/TecfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.rabelo.tecfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabelo/tecfood/TecfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabelo.tecfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
