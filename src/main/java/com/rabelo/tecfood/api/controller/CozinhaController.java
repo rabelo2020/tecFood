7584b735b079066d3d9b1d3a3ac1c6b0a387f5f7
rabelo2020 <rabelosantos2020@gmail.com>
2020-11-17 00:10:30 -0300

    Customizando algumas proriedades do Jackson

--- src/main/java/com/rabelo/tecfood/api/controller/CozinhaController.java
+++ src/main/java/com/rabelo/tecfood/api/controller/CozinhaController.java
@@ -5,24 +5,33 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.rabelo.tecfood.domain.model.Cozinha;
 import com.rabelo.tecfood.domain.repository.CozinhaRepository;
 
 @RestController
 @RequestMapping(value = "/cozinhas", 
-produces = MediaType.APPLICATION_XML_VALUE)
+produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
 public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	
+	@JsonIgnore
 	@GetMapping()
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
+	}
+	
+	@JsonIgnore
+	@GetMapping("/{id}")
+	public Cozinha buscarCozinha(@PathVariable Long id) {
+		return cozinhaRepository.buscarCozinha(id);
+		
 	}
 
 }
