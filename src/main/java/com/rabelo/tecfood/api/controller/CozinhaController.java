b5b23bb30d5a011fd2fdc67c144825734bfbd56c
rabelo2020 <rabelosantos2020@gmail.com>
2020-11-16 22:52:42 -0300

    Usando a dependencia jackson-dataformat-xml, para converter json para xml, sendo especifico qual Metodo ou Controller produces Xml

--- src/main/java/com/rabelo/tecfood/api/controller/CozinhaController.java
+++ src/main/java/com/rabelo/tecfood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.rabelo.tecfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,15 @@ import com.rabelo.tecfood.domain.model.Cozinha;
 import com.rabelo.tecfood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas", 
+produces = MediaType.APPLICATION_XML_VALUE)
 public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
 	
-	@GetMapping
+	@GetMapping()
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
 	}
