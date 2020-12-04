e18c92ad2bd758b7ccedd92e38490cdb13e91dbe
rabelo2020 <rabelosantos2020@gmail.com>
2020-12-04 00:06:16 -0300

    Corrigindo erros, ao enserir o Restaurante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,6 +18,7 @@ insert into forma_pagamento(nome) values("Dinheiro");
 
 insert into Cidade(nome, estado_id) values("Manaus", 3);
 insert into Cidade(nome, estado_id) values("Goiania", 7);
+insert into Cidade(nome, estado_id) values("Rio de Janeiro", 2);
 insert into Cidade(nome, estado_id) values("São Luis", 1);
 insert into Cidade(nome, estado_id) values("Santo André", 4); 
 insert into Cidade(nome, estado_id) values("Porto Alegre", 6);
@@ -26,26 +27,14 @@ insert into Cidade(nome, estado_id) values("São Gonçalo", 2);
 insert into Cidade(nome, estado_id) values("Cedral", 1);
 insert into Cidade(nome, estado_id) values("Campinas", 4);
 insert into Cidade(nome, estado_id) values("Teresopolis", 2);
-
-insert into Restaurante(nome, taxa_frete, cozinha_id) values ("Restaurate Baiahna", 15.03, 2);
-insert into Restaurante(nome, taxa_frete, cozinha_id) values ("Restaurate Indiana", 11.87, 4);
-insert into Restaurante(nome, taxa_frete, cozinha_id) values ("Restaurate India", 11.87, 4);
-insert into Restaurante(nome, taxa_frete, cozinha_id) values ("Restaurate La Boca", 5.21, 5);
-insert into Restaurante(nome, taxa_frete, cozinha_id) values ("Restaurate Baiahna", 15.03, 2);
-insert into Restaurante(nome, taxa_frete, cozinha_id) values ("Restaurate New York", 17.92, 3);
-insert into Restaurante(nome, taxa_frete, cozinha_id) values ("Restaurate Carioca ", 2.98, 2);
-insert into Restaurante(nome, taxa_frete, cozinha_id) values ("Restaurate Japa", 7.03, 1);
-insert into Restaurante(nome, taxa_frete, cozinha_id) values ("Comida Nordestina", 2.52, 2);
+insert into Cidade(nome, estado_id) values("São Paulo", 4);
 
 
+insert into Restaurante(nome, taxa_frete, cozinha_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero, endereco_cidade_id) values ("Comida Goiana", 2.52, 2, "Campo Grande", "23073-500", "Dois Andares", "Rua Cristalandia", "35", 3);
 
+insert into Restaurante(nome, taxa_frete, cozinha_id, endereco_bairro, endereco_cep, endereco_logradouro, endereco_numero, endereco_cidade_id) values ("Restaurate Indiana", 4.82, 4, "São Bernado", "62892-827", "Rua Das Flores", "629", 12);
+insert into Restaurante(nome, taxa_frete, cozinha_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero, endereco_cidade_id) values ("Restaurante do Japa", 13.87, 1, "Centro", "86301-380", "Fachada Verde", "Estrada chique", "76", 6);
+insert into Restaurante(nome, taxa_frete, cozinha_id, endereco_bairro, endereco_cep, endereco_complemento, endereco_logradouro, endereco_numero, endereco_cidade_id) values ("Restaurante La Plata", 36.98, 5, "São Francisco", "83602-983", "Bandeira da Argentina", "Rua Altos", "7", 4);
 
-
-
-
-
-
-
-
-
+insert into Restaurante_forma_pagamento(restaurante_id, forma_pagamento_id) values(1, 3), (1, 1), (4, 2), (2, 1), (2, 3), (3, 1), (3, 2), (3, 3);
 
